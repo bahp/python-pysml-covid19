ad0e42680a12b20ecf21f21264312f369ef5801a
Imperial College <cbit@dyn3129-88.wlan.ic.ac.uk>
2020-07-24 16:10:37 +0100

    - deleted files

--- data-report-biomarker-availability.py
+++ data-report-biomarker-availability.py
@@ -6,21 +6,30 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+"""
+   .. todo: Generalis method and just format reading the dataframe.
+"""
+
 # ---------------------
 # Configuration
 # ---------------------
 sns.set(style="whitegrid")
 
+color = 'r'
+label = 'micro_confirmed'
+
 # -----------------
 # Read data
 # -----------------
 # Load pandas dataframe
-data = pd.read_csv('./data/covid19/one_line_values_pathology.csv')
+#data = pd.read_csv('./data/covid19/one_line_values_pathology.csv')
+data = pd.read_csv('./data/covid19/daily_profiles.csv')
 # Columns to lowercase
 data.columns = [c.lower() for c in data.columns]
 
 # General variables
-biomarkers = data.columns[5:]
+biomarkers = ['micro_confirmed'] + list(data.columns[2:-2])
+#biomarkers = list(data.columns[5:])
 n_profiles = len(data)
 n_biomarkers = (len(biomarkers))
 
@@ -37,10 +46,11 @@ print("Number of biomarkers: %s" % n_biomarkers)
 # ----------------
 dataframe = pd.DataFrame()
 dataframe['count'] = data.count()
-dataframe['count_covid19'] = data[data['covid_confirmed']].count()
+dataframe['count_covid19'] = data[data[label]].count()
 dataframe['abbrev'] = dataframe.index
 dataframe = dataframe.sort_values('count', ascending=False)
-dataframe = dataframe.iloc[4:]
+dataframe = dataframe.iloc[3:]
+#dataframe = dataframe.iloc[4:]
 
 # Plot figure
 # -----------
@@ -53,12 +63,12 @@ f, ax = plt.subplots(figsize=(6, 10))
 # Plot the total availability
 sns.set_color_codes("pastel")
 sns.barplot(x='count', y='abbrev', data=dataframe.head(top),
-            label='Total', color='b')
+            label='Total', color=color)
 
 # Plot the covid19 positive availability
 sns.set_color_codes("muted")
 sns.barplot(x='count_covid19', y='abbrev', data=dataframe.head(top),
-            label="Covid19+", color="b")
+            label="Micro+", color=color)
 
 # Add a legend and informative axis label
 ax.legend(ncol=2, loc="lower right", frameon=True)
@@ -73,7 +83,7 @@ plt.title('#Profiles: %s | #Biomarkers: %s' % (len(data), len(data.columns[5:]))
 plt.tight_layout()
 
 # Save
-plt.savefig('./outputs/_figures/biomarker-availability-%s.jpg' % time.strftime("%Y%m%d%H%M%S"))
+plt.savefig('./outputs/_figures/biomarker-availability-%s.png' % time.strftime("%Y%m%d%H%M%S"))
 
 # Display
 plt.show()
